60484c11dac17ebf5c6b0e4f1f0570a2687a3468
iSanyok <supergulman09@gmail.com>
2021-02-15 17:16:13 +0300

    delete login/register methods from homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,16 +30,6 @@ class HomeController extends Controller
         return view('index', compact('articles'));
     }
 
-    public function login()
-    {
-        return view('auth.login');
-    }
-
-    public function register()
-    {
-        return view('auth.register');
-    }
-
     public function profile($id)
     {
         $author = User::find($id);
