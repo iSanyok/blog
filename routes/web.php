ee769c4f1c94b1d153a03097952e5c6b98b2bc0f
iSanyok <supergulman09@gmail.com>
2021-03-08 20:34:15 +0300

    format web.php

--- routes/web.php
+++ routes/web.php
@@ -17,24 +17,53 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::get('/', [HomeController::class, 'index'])
+    ->name('index');
 
 Route::prefix('profile')->group(function () {
-    Route::get('/{id}', [HomeController::class, 'profile'])->name('profile');
-    Route::post('/subscribe/{id}', [HomeController::class, 'subscribe'])->name('subscribe')
+    Route::get('/{id}', [HomeController::class, 'profile'])
+        ->name('profile');
+
+    Route::post('/subscribe/{id}', [HomeController::class, 'subscribe'])
+        ->name('subscribe')
         ->middleware('auth');
-    Route::delete('/unsubscribe/{id}', [HomeController::class, 'unsubscribe'])->name('unsubscribe')
+
+    Route::delete('/unsubscribe/{id}', [HomeController::class, 'unsubscribe'])
+        ->name('unsubscribe')
         ->middleware('auth');
 });
 
 Route::prefix('article')->group(function () {
-    Route::get('/show/{id}', [ArticleController::class, 'show'])->name('show');
-    Route::post('add/comment/{id}', [ArticleController::class, 'storeComment'])->name('storeComment');
-    Route::get('/add', [ArticleController::class, 'add'])->middleware('auth')->name('add');
-    Route::post('/store', [ArticleController::class, 'store'])->name('store');
-    Route::get('/edit/{id}', [ArticleController::class, 'edit'])->middleware('auth')->name('edit');
-    Route::put('/update/{id}', [ArticleController::class, 'update'])->middleware('auth')->name('update');
-    Route::delete('/delete/{id}', [ArticleController::class, 'destroy'])->middleware('auth')->name('destroy');
-    Route::post('/like/{id}', [ArticleController::class, 'like'])->middleware('auth')->name('like');
-    Route::delete('/dislike/{id}', [ArticleController::class, 'dislike'])->middleware('auth')->name('dislike');
+    Route::get('/show/{id}', [ArticleController::class, 'show'])
+        ->name('show');
+
+    Route::post('add/comment/{id}', [ArticleController::class, 'storeComment'])
+        ->name('storeComment');
+
+    Route::get('/add', [ArticleController::class, 'add'])
+        ->middleware('auth')
+        ->name('add');
+
+    Route::post('/store', [ArticleController::class, 'store'])
+        ->name('store');
+
+    Route::get('/edit/{id}', [ArticleController::class, 'edit'])
+        ->middleware('auth')
+        ->name('edit');
+
+    Route::put('/update/{id}', [ArticleController::class, 'update'])
+        ->middleware('auth')
+        ->name('update');
+
+    Route::delete('/delete/{id}', [ArticleController::class, 'destroy'])
+        ->middleware('auth')
+        ->name('destroy');
+
+    Route::post('/like/{id}', [ArticleController::class, 'like'])
+        ->middleware('auth')
+        ->name('like');
+
+    Route::delete('/dislike/{id}', [ArticleController::class, 'dislike'])
+        ->middleware('auth')
+        ->name('dislike');
 });
